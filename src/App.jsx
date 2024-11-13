0a90f83b531dd40623a61eca956151550f622fb9
Fariha <farihaorna@gmail.com>
2024-11-13 17:39:20 -0500

    added navbar component and its individual css file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import './App.css'
+const App = () => {
+  return (<div>
+  <Navbar></Navbar>
+  <p>Hello</p>
+  </div>)
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+const Navbar = () => {
+
+  return (
+    <nav className="decorative-nav">
+  </nav>
+  )
+}
+
+export default Navbar
