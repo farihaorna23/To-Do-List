858bfc4cde1db3bc74cab5d64be0f67c375b8e75
Fariha <farihaorna@gmail.com>
2024-11-14 20:02:14 -0500

    added buttons and input box

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ import './App.css'
 const App = () => {
   return (<div>
   <Navbar></Navbar>
-  <p>Hello</p>
+  <div className="container">
+  <div className="row add-task-container">
+    <div className="col-8">
+      <input type="text" placeholder='Add A Task'></input>
+    </div>
+    <div className="col-2">
+     <p>Add</p>
+    </div>
+    <div className="col-2 text-center">
+    <p>Clear All</p>
+    </div>
+  </div>
+</div>
   </div>)
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <nav className="decorative-nav">
-    <span className='title'>To Do App <i class="fa-solid fa-pencil"></i></span>
+    <span className='title'>To Do App <i className="fa-solid fa-pencil"></i></span>
   </nav>
   )
 }
